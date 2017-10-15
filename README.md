b574758ac1c9473abc7d4cbea6e978238fb6348b
KobyKotiv <kobykotiv@users.noreply.github.com>
2017-10-15 16:21:02 -0400

    First Pre Alpha Version
    Added files to repo.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # DeepBird
-DeepBird is a Recurrent Neural Network and automated twitter bot!
+DeepBird is a completely automated Recurrent Neural Network and twitter bot! My goal is to make it easier to create autonomous and intelligent Twitter bots.
+
+### To Do List:
+* Make DeepBird easier to use.
+* Write Docs.
+* Make this more modular.
+* Add Sentience Mode (Tweets and evaluates over CPU, endless Training over GPU.)
+* Use a json based config file, to allow tweaking of setting while server is running.
+* Clean up the code.
+* Add the ability to create profiles and assign a specific model.
+
+## Requirements:
+* python 3.6.x or newer.
+* Tensorflow 1.1.0rc0 (GPU version recommended).
+* Tweepy.
+* Datetime.
+* Word-RNN (You can also use Char-RNN).
+* Twitter account with an app attached to it.
+
+### Optional, but recommended:
+* Google sheet with twitter archiver enabled. (To record all tweets made by the account.)
+
+## Basic Usage
+To train with default parameters on the tinyshakespeare corpus, run:
+`python train.py `
+
+To sample from a trained model
+
+`python sample.py`
+
+To pick using beam search, use the --pick parameter. Beam search can be further customized using the --width parameter, which sets the number of beams to search with. For example:
+
+`python sample.py --pick 2 --width 4`
+
+## Automated Usage (Launch TwitterBot)
+After you configure the bot in `TwitterBot.py` open a cmd, or terminal and run:
+`python Twitterbot.py`
---
+++ TwitterBot.py
@@ -0,0 +1,124 @@
+import os
+import sys
+import time
+import tweepy
+import schedule
+import sample
+import datetime
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+playMode = 2 # 0 preview mode, 1 post on timer, 2 post on timer, 3 post on schedule.
+scheduleTime = "10:00" #00:00 midnight, 06:00 6 o'clock, 12:00 noon, etc.
+timerTime = 30 # in minutes. Must be more than 5.
+model_dir = "./save"
+
+
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = 'XxxxXxxxX'								#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = 'XxxxXxxxX'							#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = 'XxxxXxxxX'								#keep the quotes, replace this with your access token
+ACCESS_SECRET = 'XxxxXxxxX'								#keep the quotes, replace this with your access token secret
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+def main():
+	while True:
+		i = 1
+		if playMode == 0:
+			while True:
+				print("Preview mode 0 activated! Please wait...")
+				time.sleep(timerTime * 1)
+				a = "Hello World!"
+				if len(a) <= 140:
+					print("Pushing tweet!")
+					MakeTweet(a,False,i)
+				else:
+					print("tweetstring is too long. Trying again...")
+					main()
+				i+=1
+		elif playMode == 1:
+			while True:
+				print("Preview mode 1 activated! Please wait...")
+				time.sleep(timerTime * 1)
+				a = GenerateTweet()
+				numa = int(a.rindex('.')) + 1
+				a = str(a[0:140])
+				a = str(a[0:numa]).lower()
+				print('TWEE:',a)
+				print('CHAR:',len(a))
+				print('WORD:',len(str.split(a)))
+				if len(a) <= 140:
+					print("Pushing tweet!")
+					MakeTweet(a,False,i)
+				else:
+					print("tweetstring is too long. Trying again...")
+					main()
+				i+=1
+		elif playMode == 2:
+			while True:
+				print("Post on timer mode activated! Please wait...")
+				time.sleep(timerTime * 1)
+				a = GenerateTweet()
+				numa = int(a.rindex('.')) + 1
+				a = str(a[0:140])
+				a = str(a[0:numa]).lower()
+				print('TWEE:',a)
+				print('CHAR:',len(a))
+				print('WORD:',len(str.split(a)))
+				if len(a) <= 140:
+					print("Pushing tweet!")
+					MakeTweet(a,True,i)
+				else:
+					print("tweetstring is too long. Trying again...")
+					main()
+				time.sleep(timerTime * 60)
+				i+=1
+		elif playMode == 3:
+			while True:
+				print("Post on schedule mode activated! Please wait...")
+				time.sleep(timerTime * 60)
+				a = GenerateTweet()
+				print('TWEE:',a)
+				print('CHAR:',len(a))
+				print('WORD:',len(str.split(a)))
+				if len(a) <= 140:
+					print("Pushing tweet!")
+					MakeTweet(a,True,i)
+				else:
+					print("tweetstring is too long. Trying again...")
+					main()
+				i+=1
+		else:
+			print("Error! playMode must be set to either 0,1,2,3")
+	# else:
+	# 	print("Trying Again.")
+	# 	main()
+
+	# print("@KaleBobRoss just tweeted, '" + tweetstring + "'")
+	# api.update_status(line)
+	# time.sleep(900)
+
+def MakeTweet(tweetstring,rlyTweet,i):
+	currentTime = str(datetime.datetime.now())
+	print(str(i),"@KaleBobRoss just tweeted, '" + tweetstring + "' on " + currentTime)
+	if rlyTweet:
+		api.update_status(tweetstring)
+	else:
+		print("HAHA! This was only practice! Nothing was posted!")
+
+def GenerateTweet():
+	cmdl = str('--save_dir ' + model_dir + ' --pick 2 --width 4')
+	os.system(str('python3 sample.py ' + cmdl))
+	with open('TMPMSG.txt','r',encoding='utf8') as f:
+		a = f.read()
+	return a
+
+####################
+### MAIN PROGRAM ###
+####################
+
+if __name__ == '__main__':
+	main()
